cae567786798458a41a63c0c341c59ed9e328f64
Hasan-Alsulaiman <careerforma@gmail.com>
2020-06-09 16:23:49 +0300

    removed secret info

--- newBotty.py
+++ newBotty.py
@@ -8,13 +8,16 @@ def bop(bot,chatid,msg):
     bot.send_message(chat_id=chatid, text=msg)
 
 def main():
-    # out bot tokken
+    # bot tokken
     token = '1145900772:AAF9OtRgU9CQ2wb-Fiz2kYUhrg5Cm2xNFa0'
+    # group id
+    groupid=-435312318
     # creating the bot
     bot = telegram.Bot(token=token)
-    # call the function bop
+    # get system name
     sysName = platform.system()
-    bop(bot,-435312318,"Your Laptop was turned on "+ sysName)
+
+    bop(bot,groupid,"Your Laptop was turned on "+ sysName)
 
     
 if __name__ == '__main__':
